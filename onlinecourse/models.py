f4c1ba2b979745718ff56e4453d344a604e819ce
Caleb Silva <106850025+CalebSilvaBr@users.noreply.github.com>
2022-10-08 19:42:45 -0300

    Update models.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -7,6 +7,7 @@ except Exception:
     sys.exit()
 
 from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 import uuid
 
 
@@ -74,6 +75,8 @@ class Lesson(models.Model):
     order = models.IntegerField(default=0)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     content = models.TextField()
+    def __str__(self):
+        return self.title
 
 
 # Enrollment model
@@ -95,26 +98,31 @@ class Enrollment(models.Model):
     rating = models.FloatField(default=5.0)
 
 
+
 # <HINT> Create a Question Model with:
     # Used to persist question content for a course
     # Has a One-To-Many (or Many-To-Many if you want to reuse questions) relationship with course
     # Has a grade point for each question
     # Has question content
     # Other fields and methods you would like to design
-#class Question(models.Model):
+class Question(models.Model):
     # Foreign key to lesson
+    
+    lessons = models.ManyToManyField(Lesson)
     # question text
+    question_text= models.CharField(max_length=200)
     # question grade/mark
-
+    grade = models.IntegerField(validators=[MaxValueValidator(100)])
     # <HINT> A sample model method to calculate if learner get the score of the question
-    #def is_get_score(self, selected_ids):
-    #    all_answers = self.choice_set.filter(is_correct=True).count()
-    #    selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-    #    if all_answers == selected_correct:
-    #        return True
-    #    else:
-    #        return False
-
+    def is_get_score(self, selected_ids):
+       all_answers = self.choice_set.filter(is_correct=True).count()
+       selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+       if all_answers == selected_correct:
+           return True
+       else:
+           return False
+    def __str__(self):
+       return self.question_text
 
 #  <HINT> Create a Choice Model with:
     # Used to persist choice content for a question
@@ -122,13 +130,18 @@ class Enrollment(models.Model):
     # Choice content
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
-# class Choice(models.Model):
+class Choice(models.Model):
+    choice_content = models.CharField(max_length=200)
+    questions = models.ManyToManyField(Question)
+    is_correct = models.BooleanField()
+    def __str__(self):
+        return self.choice_content    
 
 # <HINT> The submission model
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
+class Submission(models.Model):
+   enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+   choices = models.ManyToManyField(Choice)
 #    Other fields and methods you would like to design
